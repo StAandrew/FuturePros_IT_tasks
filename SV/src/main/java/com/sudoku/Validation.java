8ca5731ba92812fdcefd993b5db9eb5a710d0219
Andrey <52413802+StAandrew@users.noreply.github.com>
2020-06-22 11:10:48 +0300

    Task 1 done

--- SV/src/main/java/com/sudoku/Validation.java
+++ SV/src/main/java/com/sudoku/Validation.java
@@ -1,13 +1,46 @@
-package main.java.com.sudoku;
+package com.sudoku;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 class Validation {
 
-    public Validation() {
-
+    Validation() {
     }
 
-    public boolean ValidateSolution (int[][] input) {
+    boolean ValidateSolution (int[][] input) {
+        int sum = 0, testSumRows = 0, testSumColumns = 0;
+        // get sum of first row
+        for (int i = 0; i < input[0].length; i++) {
+            sum += input[0][i];
+        }
 
-        return false;
+        // testing each row
+        for (int row = 0; row < input.length; row++) {
+            for (int col = 0; col < input[row].length; col++) {
+                testSumRows += input[row][col];
+                testSumColumns += input[col][row];
+            }
+            if (testSumRows != sum || testSumColumns != sum)
+                return false;
+            testSumRows = 0;
+            testSumColumns = 0;
+        }
+
+        // edge cases
+        ArrayList<Integer> testingCollection= new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            testingCollection.add(i);
+            for (int row = 0; row < input.length; row++) {
+                int amount = Collections.frequency(Collections.singletonList(Arrays.stream(input[0]).boxed().collect(Collectors.toList())), testingCollection);
+                if (amount > 1)
+                    return false;
+            }
+            testingCollection.clear();
+        }
+
+        return true;
     }
 }
