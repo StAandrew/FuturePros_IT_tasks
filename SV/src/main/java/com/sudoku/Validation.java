03be53f6168051bcf05c826c263b1c597a58da0e
Andrey <52413802+StAandrew@users.noreply.github.com>
2020-06-22 11:10:43 +0300

    Task 1 done

---
+++ SV/src/main/java/com/sudoku/Validation.java
@@ -0,0 +1,13 @@
+package main.java.com.sudoku;
+
+class Validation {
+
+    public Validation() {
+
+    }
+
+    public boolean ValidateSolution (int[][] input) {
+
+        return false;
+    }
+}
